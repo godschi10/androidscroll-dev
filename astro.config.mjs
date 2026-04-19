3735e687a9603994b1e690adc0b669dfda3fe25e
godschi10 <godschi10@gmail.com>
2026-04-19 18:14:05 +0100

    more page speed insights error fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,9 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://androidscroll.com',
+  build: {
+    inlineStylesheets: 'auto',
+  },
   vite: {
     plugins: [tailwindcss()]
   },
